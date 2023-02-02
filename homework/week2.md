c67b042e5013e40fe7656a9c63be572f9a010ca3
bsenst <benjamin.senst@gmail.com>
2023-02-02 22:44:42 +0100

    update etl script

--- homework/week2.md
+++ homework/week2.md
@@ -1,12 +1,18 @@
 
 # Question 1. Load January 2020 data
 
+How many rows does that dataset have?
+
 	In [10]: df.shape
 	Out[10]: (447770, 20)
 
 # Question 2. Scheduling with Cron
 
+create a deployment to run on the first of every month at 5am UTC
 
+https://docs-v1.prefect.io/core/concepts/schedules.html#cron-clocks
+
+	0 0 5 1 *
 
 # Question 3. Loading data to BigQuery
 
--- prefect/etl_web_to_gcs.py
+++ prefect/etl_web_to_gcs.py
@@ -24,14 +24,16 @@ def load(df):
     bucket_name = "dtc-de-nyctaxi-bucket-20230131"
 
     bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob("green_tripdata_2020-01.csv")
+    blob = bucket.blob("yellow_tripdata_2019-02.csv")
     
     blob.upload_from_string(df.to_csv(), 'text/csv')
 
+schedule = IntervalSchedule(interval=timedelta(minutes=2))
+
 @flow
-def flow_taxi():
-    df = extract("https://github.com/DataTalksClub/nyc-tlc-data/releases/download/green/green_tripdata_2020-01.csv.gz")
+def flow_taxi_to_gcs():
+    df = extract("https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2019-02.csv.gz")
     df_transformed = transform(df)
     load(df_transformed)
 
-flow_taxi()
+flow_taxi_to_gcs()
