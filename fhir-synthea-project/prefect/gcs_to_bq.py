ba9e9eaf10c05df23f0501bf44b3aeb95eb10c54
bsenst <benjamin.senst@gmail.com>
2023-03-10 20:05:42 +0100

    Update gcs_to_bq.py

--- fhir-synthea-project/prefect/gcs_to_bq.py
+++ fhir-synthea-project/prefect/gcs_to_bq.py
@@ -1,8 +1,23 @@
-from google.cloud import bigquery
+from google.cloud import bigquery, storage
+
+# https://cloud.google.com/storage/docs/samples/storage-list-files#storage_list_files-python
+def list_blobs(bucket_name):
+    """Lists all the blobs in the bucket."""
+    # bucket_name = "your-bucket-name"
+
+    storage_client = storage.Client()
+
+    # Note: Client.list_blobs requires at least package version 1.17.0.
+    blobs = storage_client.list_blobs(bucket_name)
+
+    # Note: The call returns a response only when the iterator is consumed.
+    # for blob in blobs:
+    #     print(blob.name)
+    return [blob.name for blob in blobs]
 
 client = bigquery.Client()
 
-table_id = "fhir-project-20230308.fhir_bq_dataset_20230803.allergies"
+bq_dataset = "fhir-project-20230308.fhir_bq_dataset_20230803"
 
 job_config = bigquery.LoadJobConfig(
     autodetect=True,
@@ -10,13 +25,17 @@ job_config = bigquery.LoadJobConfig(
     source_format=bigquery.SourceFormat.CSV,
 )
 
-uri = "gs://fhir-bucket-20230308/allergies.csv"
+bucket = "fhir-bucket-20230308"
 
-load_job = client.load_table_from_uri(
-    uri, table_id, job_config=job_config
-)
+for csv in list_blobs(bucket):
+    uri = "gs://"+bucket+"/"+csv
+    table_id = bq_dataset+"."+csv[:-4]
 
-load_job.result()
+    load_job = client.load_table_from_uri(
+        uri, table_id, job_config=job_config
+    )
 
-destination_table = client.get_table(table_id)
-print("Loaded {} rows.".format(destination_table.num_rows))
+    load_job.result()
+
+    destination_table = client.get_table(table_id)
+    print("Loaded {} with {} rows.".format(csv, destination_table.num_rows))
